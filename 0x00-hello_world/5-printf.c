c97c7a27aa7866c3287e2cf4d3c2cd223e07e42e
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-07 19:36:28 +0100

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
+  * main - A function that print message
   *
-  *
+  * Return: 0 to indicate success
   */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"):
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
