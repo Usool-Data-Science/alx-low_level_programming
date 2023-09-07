66a62117d966450fed2ef81fe006b5c7f4fa0fde
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-07 18:49:29 +0100

    update

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Prints some string
+  *
+  * Return: Always return 0
+  */
+int main(void)
+{
+	char str1[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(str1);
+	return (0);
+}
