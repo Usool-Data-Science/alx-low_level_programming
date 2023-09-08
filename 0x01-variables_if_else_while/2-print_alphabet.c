d95d996b38fa4f2514f6d7e13c4c7622d33836a5
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-08 12:16:42 +0100

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *
+  *
+  *
+  */
+
+int main(void)
+{
+	int start = 97;
+	int end = 122;
+
+	for (start; start <= end; start++)
+	{
+		printf("%c", start);
+	}
+	printf("\n");
+	return (0);
+}
