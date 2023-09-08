cc777b8d6f7de130f8983d78f3da0b7df941982b
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-08 12:23:31 +0100

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
+  * main - A function that prints alphabets
   *
+  * @start - The starting point
+  * @end - The end of print
   *
-  *
+  * Return: 0 for sucessful exit
   */
-
 int main(void)
 {
 	int start = 97;
