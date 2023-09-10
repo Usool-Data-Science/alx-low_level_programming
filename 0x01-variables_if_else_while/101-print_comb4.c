c7415eeb302f306523006361c11f9c4370ef2e38
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-10 14:58:16 +0100

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  * main - Print 3 combinations
+  *
+  * Return: 0 for success
+  */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x <= 55; x++)
+	{
+		for (y = 49; y <= 56; y++)
+		{
+			for (z = 50; z <= 57; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56 || z != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
