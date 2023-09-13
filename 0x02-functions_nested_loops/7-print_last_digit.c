28a5e5b698cc4c32007b12fffa0784d457fba02b
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-13 14:49:40 +0100

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * print_last_digit - Entry point for our code
+  *
+  8 @n: value to check
+  * Return: 0 for successful exit
+  */
+
+int print_last_digit(int n)
+{
+	int result = n % 10;
+	return (result);
+}
