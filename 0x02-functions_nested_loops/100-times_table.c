5edea72979924ce943818422985752f45de12839
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-13 21:47:37 +0100

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -19,16 +20,16 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(',');	
-					_putchar(' ');	
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(k + '0');	
+					_putchar(' ');
+					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_putchar(' ');	
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
@@ -46,4 +47,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
