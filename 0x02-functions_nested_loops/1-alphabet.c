f5f425cc6f33763e073bbee58cc0d26162fdb24a
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-13 12:49:32 +0100

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * main - Entry point for our code
+  *
+  * Return: 0 for successful exit
+  */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
