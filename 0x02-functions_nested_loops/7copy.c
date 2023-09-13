db5494277a0b6ec4d99d83dd9592d1bdff4e9fc6
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-13 15:06:42 +0100

    update

--- 0x02-functions_nested_loops/7copy.c
+++ 0x02-functions_nested_loops/7copy.c
@@ -5,20 +5,10 @@
   8 @n: value to check
   * Return: 0 for successful exit
   */
+
 int print_last_digit(int n)
 {
-	int a;
-
-	if (n < 0)
-	{
-		n = -n;
-	}
-	a = n % 10;
-	if (a < 0)
-	{
-		a = -a;
-	}
-	_putchar(a + '0');
-	return (a);
+	int result = abs(n % 10);
+	_putchar(result + '0');
+	return (result);
 }
-
