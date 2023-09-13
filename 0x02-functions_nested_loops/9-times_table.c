3f1263d4305f19f71e2e0a6e19388ecda81ea999
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-13 17:59:32 +0100

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+  * times_table  - return times table
+  *
+  * Return: 0 for successful exit
+  */
+
+void times_table(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			int result = i * j;
+
+			if (result <= 9)
+			{
+				_putchar(result + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (result > 9 && j == 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				_putchar('$');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
