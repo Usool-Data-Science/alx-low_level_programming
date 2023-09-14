b92681ae1e5e4bce16d80772ce0c670ffdfeef65
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-14 12:14:07 +0100

    update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * main - Entry point for our code
+  *
+  * Return: 0 for successful exit
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * positive_or_negative - Test if our input is zero
+  *
+  * Return: 0 for successful exit
+  */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+		return;
+	}
+	else
+	{
+		return;
+	}
+}
