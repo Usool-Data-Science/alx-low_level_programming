1cc260bbd8d087af41e2c7e113e6109e0914f113
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-14 22:31:51 +0100

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * maul - multiplies 2 integers
+  *
+  * Return: 0 for successful exit
+  */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
