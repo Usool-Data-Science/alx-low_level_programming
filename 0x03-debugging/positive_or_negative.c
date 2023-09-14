4de8956e4e968a76b308b3e2d150fbace070c690
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-14 12:25:20 +0100

    update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,6 +12,16 @@ void positive_or_negative(int i)
 		printf("%d is zero\n", i);
 		return;
 	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+		return;
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+		return;
+	}
 	else
 	{
 		return;
