be88cd5f294268cd340594e3f902bf828f8ba128
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-15 01:11:33 +0100

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  * print_triangle - print a triangle
+  *
+  * @size: number of times to print triangle
+  *
+  * Return: 0 for successful exit
+  */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
