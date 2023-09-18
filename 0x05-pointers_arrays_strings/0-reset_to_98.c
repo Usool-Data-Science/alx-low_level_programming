f8f9a4f50aff9b8ff1db4708d2ba1ee82f0b5ef3
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-18 22:36:07 +0100

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * reset_to_98 - an integer reseter for an incoming pointer
+  * @p: A pointer to convert
+  *
+  * Return: 0 for success
+  */
+
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
