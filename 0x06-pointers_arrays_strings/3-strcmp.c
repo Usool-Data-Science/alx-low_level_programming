c57156ba17045a3f9f79d2232d2494168fdb32e9
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-21 16:58:50 +0100

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,43 +6,17 @@
   *
   * Return: 0 for successful exit
   */
-
 int _strcmp(char *s1, char *s2)
 {
-	int dlen = 0;
-	int slen = 0;
-	int check = 0;
-	int i;
+	int i = 0;
 
-	while (s1[dlen] != '\0')
+	do
 	{
-		dlen++;
-	}
-	while (s2[slen] != '\0')
-	{
-		slen++;
-	}
-	if (dlen == slen)
-	{
-		for (i = 0; i < dlen; i++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] == s2[i])
-			{
-				check++;
-			}
+			return (s1[i] - s2[i]);
 		}
-		if (check == dlen)
-		{
-			return (0);
-		}
-	}
-	if (dlen < slen)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (15);
-	}
+		i++;
+	}while (s1[i] != '\0' && s2[i] != '\0');
 	return (0);
 }
