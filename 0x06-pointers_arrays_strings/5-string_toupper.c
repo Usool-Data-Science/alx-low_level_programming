acf1804db3e352d5b16201927688e0aad16fc1c8
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-09-21 17:12:58 +0100

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
