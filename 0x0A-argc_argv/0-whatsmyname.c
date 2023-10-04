61d3d7d6c5d15d7db417f024ef935f3267a944c1
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-04 10:00:36 +0100

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - The entry point
+  * @argc: count of arguments
+  * @argv: argument vector
+  *
+  * Return: 0 for successful exit
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
