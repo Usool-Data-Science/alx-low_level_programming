aab259c41b4f4c93ae5ea886f48a4b87f0a0a9ee
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-04 21:16:36 +0100

    update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+  * str_concat - string concatinator
+  * @s1: first string
+  * @s2: second string
+  *
+  * Return: 0 for successful exit
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size, i;
+	char *S1, *S2;
+	char *newstr;
+	int len1 = 0, len2 = 0;
+
+	S1 = s1;
+	S2 = s2;
+
+	while (*S1 != '\0')
+	{
+		len1++;
+		S1++;
+	}
+	while (*S2 != '\0')
+	{
+		len2++;
+		S2++;
+	}
+
+	size = len1 + len2;
+	newstr = malloc(len1 + len2 + 1);
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < len1; i++)
+		{
+			*(newstr + i) = s1[i];
+		}
+		for (; i < size; i++)
+		{
+			*(newstr + i) = s2[i];
+		}
+		*(newstr + size) = '\0';
+	}
+	return (newstr);
+}
