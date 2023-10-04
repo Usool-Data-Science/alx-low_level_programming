db30142f44792d1c4e96d28cbe1d43dc15f96e84
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-04 10:23:42 +0100

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - Entry point for our code
+  * @argc: argument count
+  *
+  * Return: 0 for successful exit
+  */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int size = argc - 1;
+
+	printf("%d\n", size);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+#define UNUSED(x) void(x)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+#endif
