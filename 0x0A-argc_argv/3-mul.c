d37b125013aceedbcb9ef56f13f2a82bd942f0fd
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-04 10:41:09 +0100

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * main - Entry point for our code
@@ -10,6 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-	
+	int mul;
+
+	if (argc >= 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error \n");
+	}
 	return (0);
 }
