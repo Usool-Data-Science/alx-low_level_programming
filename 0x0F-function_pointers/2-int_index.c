1fdf3bceb9abd1892dabc21cae62ec3597eda646
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-11 21:03:41 +0100

    Solution to Question 3

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * int_index - A function to search for an integer
+  * @array: The array with integers to search
+  * @size: Size of the array
+  * @cmp: The function for comparing the array element with a given integer
+  * Return: -1 if size <= 0 or index of the found integer
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, comp_result;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		comp_result = (*cmp)(array[i]);
+		if (comp_result != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
