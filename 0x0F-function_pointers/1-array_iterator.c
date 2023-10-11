a89a8c52aeaa61c4fba96fa85d771a8a6cf9342c
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-11 20:38:16 +0100

    Solution to Question 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * array_iterator - A function to go over each element of an array
+  * @array: The proposed array to iterate over
+  * @size: Size of our proposed array
+  * @action: The function to execute for each element of our proposed array.
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
