891dcd2013ef0bddc543ceb51e6cfa665d59e94c
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-11 04:56:41 +0100

    Solve Question 2 - Initialization example

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+  * init_dog - dog structure initializer
+  * @d: address to the dog structure
+  * @name: Name of the dog
+  * @age: Age of the dog
+  * @owner: Name of the dog owner
+  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
