95f50c9c8e8259754ba94869d6d5576b1d436bf8
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-11 05:11:25 +0100

    Solution to question 4 and 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,15 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+  * dog_t - a new alias for struct dog
+  */
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOC_H */
