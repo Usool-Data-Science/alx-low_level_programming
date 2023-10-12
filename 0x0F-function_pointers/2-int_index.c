e8a6c394bb326ec9dfbc13972a92e98bea440ad1
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-12 04:54:10 +0100

    Solution to Question 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,12 +18,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	for (i = 0; i < size; i++)
+	if (cmp)
 	{
-		comp_result = (*cmp)(array[i]);
-		if (comp_result != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			comp_result = (*cmp)(array[i]);
+			if (comp_result != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
