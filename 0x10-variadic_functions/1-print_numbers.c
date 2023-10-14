e99601598c6845e4cccba2afe77483277a62a627
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-10-14 12:22:38 +0100

    Correct Question 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,8 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int index;
-
-	va_start(nums, n);
+	if (n)
+	{
+		va_start(nums, n);
+	}
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
