dd64b74cd1894eee9ecab2eea8b9c150ca673cec
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-19 19:21:09 +0100

    Solve task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+  * print_list - A function to print the a singly linked list
+  *
+  * @h: the singly linked list to print
+  * Return: the counts of elements in the list
+  */
+
 size_t print_list(const list_t *h)
 {
 	size_t element = 0;
