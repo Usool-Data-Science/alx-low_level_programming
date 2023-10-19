432730da1b23126c2a80db0e7be8b177210de61d
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-19 21:40:13 +0100

    solve task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,15 +3,23 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+  * add_node - A function to add node to the beginning of a list
+  * @head: the first node
+  * @str: the string data for the node
+  *
+  * Return: pointer to the new node
+  */
+
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_element;
 	unsigned int length = 0;
 
-	while (*str != '\0')
+	while (str[length] != '\0')
 	{
 		length++; /* Get the size of str*/
-		str++;
 	}
 
 	new_element = (list_t *) malloc(sizeof (list_t));
