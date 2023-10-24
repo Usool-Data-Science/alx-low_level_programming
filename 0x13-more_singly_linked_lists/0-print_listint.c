c16dfa9a5a51c4fe34d45b2fef87e78fe2c2ee23
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-24 16:57:05 +0100

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,13 +14,12 @@ size_t print_listint(const listint_t *h)
 	const listint_t *current = h;
 	size_t elem_count = 0;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("%d\n", current->n);
 		elem_count++;
 		current = current->next;
 	}
-	elem_count++;
 	printf("%d\n", current->n);
 	return (elem_count);
 }
