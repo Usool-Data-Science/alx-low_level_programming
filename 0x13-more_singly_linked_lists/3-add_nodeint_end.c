94e17537b23c47914211b542d92367c70a45cec0
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-24 10:05:08 +0100

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,14 +14,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
 
-	newNode->n = n;
-
-	newNode->next = NULL;
-
-	if (!newNode)
+	if (!newNode && head)
 	{
 		return (NULL);
 	}
+	
+	newNode->n = n;
+	newNode->next = NULL;
 
 	if (*head == NULL)
 	{
