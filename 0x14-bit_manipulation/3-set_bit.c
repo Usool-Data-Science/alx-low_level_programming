3a980c4a568fd8633c9c94f77857e97792da50fe
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-26 20:25:22 +0100

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -18,7 +18,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index < bitSize)
 	{
 		result = (*n | (1 << index));
-		*n += (1 << index);
+		*n |= (1 << index);
 		return (result);
 	}
 
