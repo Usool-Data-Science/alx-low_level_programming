ec79fa47fb9bff353d2a62207b351110a7212b14
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-26 20:24:24 +0100

    Solve task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,8 +13,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int result;
+	unsigned int bitSize = sizeof(n) * 8;
 
-	if (n > 1)
+	/* Check if n is positive and the index is within bit size range*/
+	if (n > 1 && index < bitSize)
 	{
 		result = (n & (1 << index)) > 0 ? 1 : 0;
 		return (result);
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * set_bit - A function that sets the value of a bit to 1 at an index
+  * @n: A pointer to the integer.
+  * @index: The index to switch on.
+  *
+  * Return: 1 if it worked or -1 if an error occured
+  */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int result;
+	unsigned int bitSize = sizeof(*n) * 8;
+
+	if (index < bitSize)
+	{
+		result = (*n | (1 << index));
+		*n += (1 << index);
+		return (result);
+	}
+
+	return (-1);
+}
