1a9c48c602aaadbc050c4f666fe5f74bb0780d75
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-10-30 22:32:24 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,13 +22,15 @@ int create_file(const char *filename, char *text_content)
 	if (file_d == -1)
 		return (-1);
 
-	for (i = 0; text_content[i]; i++)
+	if (text_content)
 	{
-		writer = write(file_d, text_content, i);
+		writer = write(file_d, text_content, strlen(text_content);
+		if (writer == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
 	}
-
-	if (writer == -1)
-		return (-1);
 
 	close(file_d);
 	return (1);
