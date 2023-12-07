59db43193327544e982f6e51668104fa5e365027
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-12-07 13:50:42 +0100

    Solve task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - A function that prints doubly linked list
+ * @h: The head of the linked list
+ *
+ * Return: Count of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+	const dlistint_t *current = h;
+
+	if (!h)
+		exit(EXIT_FAILURE);
+
+	while (current->next != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		n++;
+	}
+	n++;
+	printf("%d\n", current->n);
+	return (n);
+}
