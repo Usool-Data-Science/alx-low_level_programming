1255bd7bd5c43a14ed2063a440d1035557bc94d1
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-12-08 01:35:16 +0100

    Solve task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,22 +1,54 @@
-#include <stdio.h>
 #include "lists.h"
 
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
-	dlistint_t *current = *head;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	if (!head || *head == NULL)
-		return (NULL);
-	if (index == 0)
+	h1 = *head;
+
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		if (current->next)
+		unsigned int current_index = i;
+		if (current_index == index)
 		{
-			*head = current->next;
-			current->next->prev = NULL;
+			if (current_index == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
+			return (1);
 		}
-
-
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
-	while (current->next && count < index)
+
+	return (-1);
 }
+
